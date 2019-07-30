1bb63c19061d5c54aee4c7542828a23daff30549
= <=>
2019-07-30 21:04:26 +0200

    prettier

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-const Pet = ({ name, animal, breed }) => { 
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]);
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {name: "Walter", animal: "Raccoon", breed: "Kinkajous"}),
-            React.createElement(Pet, {name: "Wagter", animal: "Dog", breed: "St. Bernard"}),
-        ]
-    );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Walter",
+      animal: "Raccoon",
+      breed: "Kinkajous"
+    }),
+    React.createElement(Pet, {
+      name: "Wagter",
+      animal: "Dog",
+      breed: "St. Bernard"
+    })
+  ]);
 };
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
