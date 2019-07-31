a9cf16fea2308d55afdb5f10b9380381d0ea9260
= <=>
2019-07-31 20:41:59 +0200

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Walter" animal="Raccoon" breed="Kinkajous" />
-      <Pet name="Wagter" animal="Dog" breed="St. Bernard" />
+      <SearchParams />
     </div>
   );
 };
---
+++ src/SearchParams.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const SearchParams = function() {
+  const [location, setLocation] = useState("");
+  return (
+    <div className="search-params">
+      <form action="">
+        <label htmlFor="location">
+          Location
+          <input
+            type="text"
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
