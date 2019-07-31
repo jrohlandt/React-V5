e6bbacccb0dafb8fc4e585d7ec0fa5e1b8329d38
= <=>
2019-07-31 20:13:10 +0200

    jsx

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Walter",
-      animal: "Raccoon",
-      breed: "Kinkajous"
-    }),
-    React.createElement(Pet, {
-      name: "Wagter",
-      animal: "Dog",
-      breed: "St. Bernard"
-    })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Walter" animal="Raccoon" breed="Kinkajous" />
+      <Pet name="Wagter" animal="Dog" breed="St. Bernard" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
