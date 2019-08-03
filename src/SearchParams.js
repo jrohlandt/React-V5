bfdea59c20eeacafde007450d31f1d4758844c3b
= <=>
2019-08-03 10:49:19 +0200

    useEffect and strict mode

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,24 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = function() {
   const [location, setLocation] = useState("");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    if (animal.length === 0) return;
+
+    pet.breeds(animal).then(res => {
+      const breedNames = res.breeds.map(b => b.name);
+      setBreeds(breedNames);
+    }, console.error);
+  }, [setBreeds, setBreed, animal]);
 
   return (
     <div className="search-params">
